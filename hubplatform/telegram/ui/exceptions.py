6304592dfdfeb4e1df4cc94bee47aad9a59d2a9a
qvvonk <qvvonk@gmail.com>
2026-08-17 13:56:51 +0300

    added errors lists

--- hubplatform/telegram/ui/exceptions.py
+++ hubplatform/telegram/ui/exceptions.py
@@ -4,7 +4,10 @@ from __future__ import annotations
 __all__ = [
     'TelegramUIError',
     'MenuBuildingError',
+    'MenuFinalizingError',
     'KeyboardBlockBuildingError',
+    'KeyboardBlockModificationError',
+    'ButtonRenderError',
 ]
 
 from hubplatform.exceptions.base import HubPlatformError
@@ -13,7 +16,41 @@ from hubplatform.exceptions.base import HubPlatformError
 class TelegramUIError(HubPlatformError): ...
 
 
-class MenuBuildingError(TelegramUIError): ...
+class MenuBuildingError(TelegramUIError):
+    def __init__(self, *, menu_id: str, message: str | None = None) -> None:
+        self.menu_id = menu_id
+        message = message if message is not None else f'Failed to build menu {menu_id!r}.'
+        super().__init__(message)
 
 
-class KeyboardBlockBuildingError(TelegramUIError): ...
+class MenuFinalizingError(MenuBuildingError):
+    def __init__(self, *, menu_id: str, message: str | None = None) -> None:
+        message = message if message is not None else f'Failed to finalize menu {menu_id!r}.'
+        super().__init__(menu_id=menu_id, message=message)
+
+
+class KeyboardBlockBuildingError(TelegramUIError):
+    def __init__(self, block_id: str, message: str | None = None) -> None:
+        self.block_id = block_id
+        message = (
+            message if message is not None else f'Failed to build keyboard block {block_id!r}.'
+        )
+        super().__init__(message)
+
+
+class KeyboardBlockModificationError(KeyboardBlockBuildingError):
+    def __init__(self, block_id: str, modification_id: str, message: str | None = None) -> None:
+        self.modification_id = modification_id
+        message = (
+            message
+            if message is not None
+            else f'Failed to run modification {modification_id!r} for keyboard block {block_id!r}.'
+        )
+        super().__init__(block_id=block_id, message=message)
+
+
+class ButtonRenderError(TelegramUIError):
+    def __init__(self, button_id: str, message: str | None = None) -> None:
+        self.button_id = button_id
+        message = message if message is not None else f'Failed to render button {button_id!r}.'
+        super().__init__(message)
--- hubplatform/telegram/ui/types.py
+++ hubplatform/telegram/ui/types.py
@@ -14,16 +14,21 @@ __all__ = [
 ]
 
 from typing import Any, Self, Union, Literal, ParamSpec, Concatenate
-from dataclasses import field, dataclass
-from collections.abc import Mapping, Callable, Awaitable
+from dataclasses import field as dataclass_field, dataclass
+from collections.abc import Mapping, Callable, Awaitable, MutableSequence
 
 from pydantic import Field, BaseModel, ConfigDict
 from aiogram.types import LoginUrl, WebAppInfo, CallbackGame, CopyTextButton, InlineKeyboardButton
+from pydantic.dataclasses import dataclass as pydantic_dataclass
 from eventry.asyncio.callable_wrappers import CallableWrapper
 
 from hubplatform.logging.loggers import telegram as _logger
 from hubplatform.telegram.callback_data import CallbackData
-from hubplatform.telegram.ui.exceptions import KeyboardBlockBuildingError
+from hubplatform.telegram.ui.exceptions import (
+    ButtonRenderError,
+    KeyboardBlockBuildingError,
+    KeyboardBlockModificationError,
+)
 from hubplatform.core.pydantic_serializable import pydantic_fallback_serializer
 from hubplatform.telegram.callback_data.hash import HashService
 
@@ -31,7 +36,7 @@ from hubplatform.telegram.callback_data.hash import HashService
 logger = _logger.ui
 
 _P = ParamSpec('_P')
-Keyboard = list[list['Button']]
+Keyboard = MutableSequence[MutableSequence['Button']]
 KeyboardSpecBuilder = Callable[..., Awaitable[Keyboard]] | Callable[..., Keyboard]
 KeyboardModificationCallable = Union[
     Callable[
@@ -45,6 +50,28 @@ MenuFinalizer = Union[
     Callable[Concatenate['MenuContext', 'MenuSpec', _P], Awaitable[Union['MenuSpec']]],
     Callable[Concatenate['MenuContext', 'MenuSpec', _P], 'MenuSpec'],
 ]
+
+
+def _validate_keyboard(keyboard: Keyboard) -> None:
+    if not isinstance(keyboard, MutableSequence):
+        raise TypeError(
+            f'Unexpected keyboard type. Expected: `MutableSequence[MutableSequence[Button]]`, '
+            f'got: {keyboard.__class__.__name__!r}'
+        )
+
+    for line_index, line in enumerate(keyboard):
+        if not isinstance(line, MutableSequence):
+            raise TypeError(
+                f'Unexpected buttons line type at line {line_index}. '
+                f'Expected: `MutableSequence[Button]`, got: {line.__class__.__name__!r}.'
+            )
+
+        for button_index, button in enumerate(line):
+            if not isinstance(button, Button):
+                raise TypeError(
+                    f'Unexpected button type at line {line_index} at position {button_index}. '
+                    f'Expected: `Button`, got: {button.__class__.__name__!r}.'
+                )
 
 
 class Button(BaseModel):
@@ -124,13 +151,22 @@ class Button(BaseModel):
     """
 
     def render(self, hash_service: HashService | None = None) -> InlineKeyboardButton:
+        try:
+            return self._render(hash_service=hash_service)
+        except ButtonRenderError:
+            raise
+        except Exception as e:
+            raise ButtonRenderError(button_id=self.button_id) from e
+
+    def _render(self, hash_service: HashService | None = None) -> InlineKeyboardButton:
         callback_data: str | None = None
         if self.callback_data is not None:
             if self.hash and hash_service is None:
-                raise ValueError(
-                    f'Cannot render button {self.button_id}. '
+                raise ButtonRenderError(
+                    button_id=self.button_id,
+                    message=f'Cannot render button {self.button_id}. '
                     'Button requires to hash its `callback_data`, '
-                    'but hash_service was not provided.'
+                    'but hash_service was not provided.',
                 )
 
             if isinstance(self.callback_data, CallbackData):
@@ -156,6 +192,9 @@ class Button(BaseModel):
             callback_data=callback_data,
             web_app=self.web_app,
             login_url=self.login_url,
+            copy_text=self.copy_text,
+            callback_game=self.callback_game,
+            pay=self.pay,
         )
 
 
@@ -186,46 +225,37 @@ class KeyboardBlockSpec:
             )
         )
 
-    async def build(
-        self,
-        di_context: Mapping[str, Any],
-        hash_service: HashService | None = None,
-    ) -> list[list[InlineKeyboardButton]]:
+    async def build(self, di_context: Mapping[str, Any]) -> KeyboardBlockBuildingResult:
         try:
-            initial_keyboard = await self._builder(data=di_context)
+            keyboard = await self._builder(data=di_context)
+            _validate_keyboard(keyboard)
         except Exception as e:
-            raise KeyboardBlockBuildingError(
-                f'An error occurred while building keyboard block {self.block_id!r}.'
-            ) from e
+            raise KeyboardBlockBuildingError(block_id=self.block_id) from e
 
-        buttons = initial_keyboard
+        keyboard = keyboard
         pending_mods = self._modifications.copy()
+        errors = []
         while pending_mods:
-            modification = pending_mods.pop()
+            modification = pending_mods.pop(0)
             try:
-                state = KeyboardBuildingState(buttons=buttons, pending_modifications=pending_mods)
+                state = KeyboardBuildingState(
+                    keyboard=keyboard, pending_modifications=pending_mods
+                )
                 result = await modification.build(state, di_context)
                 if isinstance(result, KeyboardBuildingState):
-                    buttons = result.buttons
+                    keyboard = result.keyboard
                     pending_mods = result.pending_modifications
                 else:
-                    buttons = result
-            except Exception:
-                logger.error(
-                    'An error occurred while executing modification %s for keyboard block %s. '
-                    'Skipping modification.',
-                    modification.modification_id,
-                    self.block_id,
-                    exc_info=True,
+                    keyboard = result
+            except KeyboardBlockModificationError as e:
+                errors.append(e)
+            except Exception as e:
+                new_e = KeyboardBlockModificationError(
+                    block_id=self.block_id, modification_id=modification.modification_id
                 )
-
-        result = []
-        for line in buttons:
-            result_line = []
-            for button in line:
-                result_line.append(button.render(hash_service=hash_service))
-            result.append(result_line)
-        return result
+                new_e.__cause__ = e
+                errors.append(new_e)
+        return KeyboardBlockBuildingResult(keyboard=keyboard, errors=errors)
 
     @classmethod
     def callback_button(
@@ -310,7 +340,7 @@ class KeyboardBlockSpec:
 
 @dataclass(frozen=True)
 class KeyboardModificationMeta:
-    _wrapped: CallableWrapper[Keyboard | KeyboardBuildingState] = field(init=False)
+    _wrapped: CallableWrapper[Keyboard | KeyboardBuildingState] = dataclass_field(init=False)
     modification_id: str
     callable: KeyboardModificationCallable
 
@@ -323,52 +353,39 @@ class KeyboardModificationMeta:
         result = await self._wrapped(args=[state], data=di_context)
         if isinstance(result, KeyboardBuildingState):
             return result
-        if isinstance(result, list):
-            for line_index, line in enumerate(result):
-                if not isinstance(line, list):
-                    raise KeyboardBlockBuildingError(
-                        f'Keyboard block modification returned '
-                        f'unexpected value typed at line {line_index}. '
-                        f'Expected: list[Button], got: {type(line).__name__!r}.'
-                    )
-                for button_index, button in enumerate(line):
-                    if not isinstance(button, Button):
-                        raise KeyboardBlockBuildingError(
-                            f'Keyboard block modification returned '
-                            f'unexpected value typed at line {line_index} '
-                            f'at position {button_index}. '
-                            f'Expected: list[Button], got: {type(line).__name__!r}.'
-                        )
-        else:
-            raise KeyboardBlockBuildingError(
-                f'Keyboard block modification {self.modification_id!r} '
-                f'returned unexpected value type. '
-                f'Expected: list[Button] or KeyboardBuildingState, got: {type(result).__name__!r}.'
-            )
-
+        _validate_keyboard(result)
         return result
 
 
 @dataclass
 class KeyboardBuildingState:
-    buttons: list[list[Button]]
+    keyboard: Keyboard
     pending_modifications: list[KeyboardModificationMeta]
 
+    def __post_init__(self) -> None:
+        _validate_keyboard(self.keyboard)
 
-class MenuSpec(BaseModel):
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True,
-    )
 
+@dataclass
+class KeyboardBlockBuildingResult:
+    keyboard: Keyboard
+    errors: list[KeyboardBlockModificationError] = dataclass_field(default_factory=list)
+
+
+@pydantic_dataclass(
+    config=ConfigDict(arbitrary_types_allowed=True, validate_assignment=True),
+    validate_on_init=True,
+)
+class MenuSpec:
     header_text: str = ''
     header_body_sep: str = '\n\n'
     body_text: str = ''
     body_footer_sep: str = '\n\n'
     footer_text: str = ''
     header_footer_sep: str = '\n\n'
-    header_keyboard: list[KeyboardBlockSpec] = field(default_factory=list)
-    main_keyboard: list[KeyboardBlockSpec] = field(default_factory=list)
-    footer_keyboard: list[KeyboardBlockSpec] = field(default_factory=list)
+    header_keyboard: MutableSequence[KeyboardBlockSpec] = dataclass_field(default_factory=list)
+    main_keyboard: MutableSequence[KeyboardBlockSpec] = dataclass_field(default_factory=list)
+    footer_keyboard: MutableSequence[KeyboardBlockSpec] = dataclass_field(default_factory=list)
     finalizer: MenuFinalizer | None = None
 
     def total_blocks(self) -> list[KeyboardBlockSpec]:
@@ -379,12 +396,39 @@ class MenuSpec(BaseModel):
         ]
 
     async def render(
-        self, app_context: Mapping[str, Any], hash_service: HashService | None = None
+        self,
+        app_context: Mapping[str, Any],
+        hash_service: HashService | None = None,
     ) -> RenderedMenu:
         keyboard = []
+        building_errors = []
         for block in self.total_blocks():
-            result = await block.build(app_context, hash_service=hash_service)
-            keyboard.extend(result)
+            try:
+                result = await block.build(app_context)
+                keyboard.extend(result.keyboard)
+                building_errors.extend(result.errors)
+            except KeyboardBlockBuildingError as e:
+                building_errors.append(e)
+            except Exception as e:
+                new_e = KeyboardBlockBuildingError(block_id=block.block_id)
+                new_e.__cause__ = e
+                building_errors.append(new_e)
+
+        converted_keyboard: list[list[InlineKeyboardButton]] = []
+        render_errors = []
+        for line in keyboard:
+            result_line = []
+            for button in line:
+                try:
+                    result_line.append(button.render(hash_service=hash_service))
+                except ButtonRenderError as e:
+                    render_errors.append(e)
+                except Exception as e:
+                    new_e = ButtonRenderError(button_id=button.button_id)
+                    new_e.__cause__ = e
+                    render_errors.append(new_e)
+            if result_line:
+                converted_keyboard.append(result_line)
 
         text = self.header_text
         if self.body_text:
@@ -398,21 +442,35 @@ class MenuSpec(BaseModel):
                 text += self.header_footer_sep
             text += self.footer_text
 
-        return RenderedMenu(text=text, keyboard=keyboard)
+        return RenderedMenu(
+            text=text,
+            keyboard=converted_keyboard,
+            building_errors=building_errors,
+            render_errors=render_errors,
+        )
 
 
-@dataclass
+@pydantic_dataclass(
+    config=ConfigDict(arbitrary_types_allowed=True, validate_assignment=True),
+)
+class PreFinalizedMenu:
+    header_text: str
+    header_body_sep: str
+    body_text: str
+    body_footer_sep: str
+    footer_text: str
+    header_footer_sep: str
+    header_keyboard: Keyboard = dataclass_field(default_factory=list)
+    main_keyboard: Keyboard = dataclass_field(default_factory=list)
+    footer_keyboard: Keyboard = dataclass_field(default_factory=list)
+
+
+@pydantic_dataclass
 class RenderedMenu:
     text: str
     keyboard: list[list[InlineKeyboardButton]]
-
-
-class MenuContextSnapshot(BaseModel):
-    menu_id: str
-    keyboard_page: int
-    text_page: int
-    data: dict[str, Any]
-    fields: dict[str, Any]
+    building_errors: list[KeyboardBlockBuildingError] = Field(default_factory=list)
+    render_errors: list[ButtonRenderError] = Field(default_factory=list)
 
 
 class MenuContext(BaseModel):
@@ -457,3 +515,11 @@ class MenuContext(BaseModel):
                 'data': envelope.data,
             }
         )
+
+
+class MenuContextSnapshot(BaseModel):
+    menu_id: str
+    keyboard_page: int
+    text_page: int
+    data: dict[str, Any]
+    fields: dict[str, Any]
