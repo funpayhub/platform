3aa06bb97182d7bb7bf79587987b0de6fd06e5a9
qvvonk <qvvonk@gmail.com>
2026-08-27 13:39:39 +0300

    small api change

--- hubplatform/plugins/__init__.py
+++ hubplatform/plugins/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from .manager import (
-    DependencyManager as DependencyManager,
-    global_dependency_manager as global_dependency_manager,
-)
 from .manifest import (
     LoadedPlugin as LoadedPlugin,
     PluginAuthor as PluginAuthor,
     PluginManifest as PluginManifest,
+)
+from .dependencies.manager import (
+    DependencyManager as DependencyManager,
+    global_dependency_manager as global_dependency_manager,
 )
