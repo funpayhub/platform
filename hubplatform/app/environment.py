9749be4e8bf719dac13eaba5aedc3b332c34fdbf
qvvonk <qvvonk@gmail.com>
2026-09-02 12:18:35 +0300

    small changes

--- hubplatform/app/environment.py
+++ hubplatform/app/environment.py
@@ -10,9 +10,17 @@ from pathlib import Path
 
 @dataclass(frozen=True)
 class AppEnvironment:
+    logs_dir: Path = field(
+        init=False,
+        default=Path(os.environ.get('HUBPLATFORM_LOGS_DIR', Path(os.getcwd()) / 'logs'))
+    )
+
     file_goods_sources_dir: Path = field(
         init=False,
         default=Path(
-            os.environ.get('FILE_GOODS_SOURCES_DIR', Path(os.getcwd()) / 'storage' / 'goods')
+            os.environ.get(
+                'HUBPLATFORM_FILE_GOODS_SOURCES_DIR',
+                Path(os.getcwd()) / 'storage' / 'goods',
+            )
         ),
     )
