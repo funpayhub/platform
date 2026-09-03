b8e3796fb1d086a79e00859320ccb9e7a58e0a63
qvvonk <qvvonk@gmail.com>
2026-09-03 10:36:48 +0300

    small changes in environment

--- hubplatform/app/environment.py
+++ hubplatform/app/environment.py
@@ -5,25 +5,26 @@ __all__ = ['AppEnvironment', 'app_environment']
 
 import os
 from dataclasses import field, dataclass
+from functools import cache
 from pathlib import Path
+
+
+_env = os.environ.get
+_LOGS_DIR = Path(_env('HUBPLATFORM_LOGS_DIR', Path(os.getcwd()) / 'logs'))
+_STORAGE_PATH = Path(_env('HUBPLATFORM_STORAGE_PATH', Path(os.getcwd()) / 'storage'))
+_FILE_GOODS_SOURCES_DIR = Path(
+    _env('HUBPLATFORM_FILE_GOODS_SOURCES_DIR',  _STORAGE_PATH / 'goods')
+)
+
 
 
 @dataclass(frozen=True)
 class AppEnvironment:
-    logs_dir: Path = field(
-        init=False,
-        default=Path(os.environ.get('HUBPLATFORM_LOGS_DIR', Path(os.getcwd()) / 'logs')),
-    )
-
-    file_goods_sources_dir: Path = field(
-        init=False,
-        default=Path(
-            os.environ.get(
-                'HUBPLATFORM_FILE_GOODS_SOURCES_DIR',
-                Path(os.getcwd()) / 'storage' / 'goods',
-            )
-        ),
-    )
+    logs_dir: Path = field(default=_LOGS_DIR)
+    storage_path: Path = field(default=_STORAGE_PATH)
+    file_goods_sources_dir: Path = field(default=_FILE_GOODS_SOURCES_DIR)
 
 
-app_environment = AppEnvironment()
+@cache
+def app_environment() -> AppEnvironment:
+    return AppEnvironment()
